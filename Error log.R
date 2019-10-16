8480f2d30a41675056d5e1781fde04ef86605810
Lauren Kennedy <lauken13@users.noreply.github.com>
2019-10-16 11:34:31 -0400

    initial data upload

---
+++ Error log.R
@@ -0,0 +1,29 @@
+#year 1
+add: expenses much bigger than income for case 423 in y1
+case 230 is a duplicate of 89 with slightly different name 
+case 29 251 372 spend no money on anything other than their pets. Case notes: Might not have pets (thus not eligible)
+cases 432, 294, 123, 65, 10 have missing values recorded as 99 for age in y1
+happiness is scored on a 0:30 scale, case 367 has value of 32 in y1
+case 227 added a zero to their age in y1
+
+#year 2
+case 230 is a duplicate of 89 with slightly different name
+case 161 324 spend no money on anything other than their pets. Case notes: Might not have pets (thus not eligible)
+case 109 has a slight last name mispelling
+gained pet has a large influx on NAs Case notes: server randomly dumping data
+
+#year 3
+cats_n_dogs is accidentally coded as 1 and 2
+Pet expenses more than yearly expenses for case 160 210 216 309
+happiness entirely negative 
+dog_registered variable name is different
+case 109 has a slight first name mispelling
+case 109 has a surge in income by a multiplcal of 52
+
+#year 4
+
+case id 356 spends 100% on their pet
+
+
+
+
